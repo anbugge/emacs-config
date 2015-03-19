f35e86c2fe8abe3baf21a7da8a1f2607ef760eda
Audun Bugge <anbugge@silabs.com>
2015-03-19 13:26:20 +0100

    Misc updates for ANB

--- setup/emacs_custom_anb.el
+++ setup/emacs_custom_anb.el
@@ -24,7 +24,13 @@
 
 ;;; Move line/selection by M-up / M-down
 (require 'move-text)
-(define-key global-map (kbd "M-<up>") 'move-text-up)
+(defun move-text-and-point-up ()
+  (interactive)
+  (move-text-up 1)
+  (previous-line))
+                             
+(define-key global-map (kbd "S-M-<up>") 'move-text-up)
+(define-key global-map (kbd "M-<up>") 'move-text-and-point-up)
 (define-key global-map (kbd "M-<down>") 'move-text-down)
 
 ;;; Comment/uncomment the current line
@@ -52,55 +58,15 @@
 
 (global-set-key (kbd "M-J") 'pull-next-line)
 
+;; Re-mapped just-one-space because meta-space is finder on mac
+(global-set-key (kbd "S-M-SPC") 'just-one-space)
 
-;;;;    CEDET is now included in Emacs
 
 ;; Load CEDET.
-;; See cedet/common/cedet.info for configuration details.
-;; IMPORTANT: For Emacs >= 23.2, you must place this *before* any
-;; CEDET component (including EIEIO) gets activated by another 
-;; package (Gnus, auth-source, ...).
-  ;(load-file "k:/tools/emacs/scripts/cedet-1.1/common/cedet.el")
+(semantic-mode 1)
 
 ;; Enable EDE (Project Management) features
-  ;(global-ede-mode 1)
-
-;; Enable EDE for a pre-existing C++ project
-;; (ede-cpp-root-project "NAME" :file "~/myproject/Makefile")
-
-
-;; Enabling Semantic (code-parsing, smart completion) features
-;; Select one of the following:
-
-;; * This enables the database and idle reparse engines
-  ;(semantic-load-enable-minimum-features)
-
-;; * This enables some tools useful for coding, such as summary mode,
-;;   imenu support, and the semantic navigator
-;; (semantic-load-enable-code-helpers)
-
-;; * This enables even more coding tools such as intellisense mode,
-;;   decoration mode, and stickyfunc mode (plus regular code helpers)
-  ;(semantic-load-enable-gaudy-code-helpers)
-
-;; * This enables the use of Exuberant ctags if you have it installed.
-;;   If you use C++ templates or boost, you should NOT enable it.
-;; (semantic-load-enable-all-exuberent-ctags-support)
-;;   Or, use one of these two types of support.
-;;   Add support for new languages only via ctags.
-;; (semantic-load-enable-primary-exuberent-ctags-support)
-;;   Add support for using ctags as a backup parser.
-;; (semantic-load-enable-secondary-exuberent-ctags-support)
-
-;; Enable SRecode (Template management) minor-mode.
-;; (global-srecode-minor-mode 1)
-
-  ;(require 'semantic-ia)
-  ;(require 'semantic-gcc)
-
-
-;;; MinGW include dir for semantic analysis
-; (semantic-add-system-include "C:/QtSDK/mingw/include/" 'c-mode)
+(global-ede-mode 1)
 
 
 ;; Custom semantic key bindings
@@ -130,7 +96,7 @@
 (add-hook 'c-mode-hook (lambda ()
                          (set (make-local-variable 'rebox-style-loop) '(245 548))
                          (set (make-local-variable 'rebox-min-fill-column) 60)
-                         (rebox-mode 1)))
+                         (rebox-mode 0)))
 
 ;; Don't load any version control stuff
 ;(setq vc-handled-backends ())
@@ -147,3 +113,30 @@
   (add-to-list 'ac-sources 'ac-source-semantic))
 (add-hook 'c-mode-common-hook 'my-c-mode-cedet-hook)
 
+
+;;
+;; Drop temporary files in $TMPDIR/emacs$UID/
+;;
+(defconst emacs-tmp-dir (format "%s/%s%s/" temporary-file-directory "emacs" (user-uid)))
+(setq backup-directory-alist
+      `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix
+      emacs-tmp-dir)
+
+
+(defun bf-pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+    (nxml-mode)
+    (goto-char begin)
+    (while (search-forward-regexp "\>[ \\t]*\<" nil t) 
+      (backward-char) (insert "\n"))
+    (indent-region begin end))
+  (message "Ah, much better!"))
